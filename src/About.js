0ae6e0193785d083aab16e4cd382ffaaef4acaa0
Manasvi Arora <manasviarora28@gmail.com>
2024-01-16 13:52:30 +0530

    useNavigate Hook

--- src/About.js
+++ src/About.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import {NavLink} from 'react-router-dom'
+import {NavLink, useNavigate} from 'react-router-dom'
 import Header from './Header'
 
 const About = () => {
+    const navigate = useNavigate();
+    const goToContact = () => {
+        navigate("/contact");
+    }
   return (
+
     <div>
-    
       <h1>About us</h1>
+      <button onClick={() => goToContact()}>Go to contact page</button>
     </div>
   )
 }
--- src/Contact.js
+++ src/Contact.js
@@ -1,12 +1,24 @@
 import React from 'react'
 
 import Header from './Header'
+import { useNavigate } from 'react-router-dom'
 
 const Contact = () => {
+    const navigate = useNavigate();
+    const goToHome = () =>{
+        navigate("/")
+        
+    }
+    const goBack = () => {
+        navigate(-1) /* Goes Back to the previous page */
+    }
   return (
+
     <div>
         
         <h1>Contact</h1>
+        <button onClick={goToHome}>go to home page</button>
+        <button onClick = {goBack}>go back</button>
     </div>
   )
 }
